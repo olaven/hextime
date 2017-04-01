ef5d3c22ad3d0971de01fcd0b9af0804ba89f6e4
olaven <olavsundfoer@gmail.com>
2017-04-02 01:44:58 +0200

    backgroundcolor to txxtarea added. Also, clock is moved slightly. Unsure of how it fits different screen sizes..

--- hextime.js
+++ hextime.js
@@ -1,7 +1,17 @@
 window.onload = pageLoaded;
 
+var string; //is global so that it can be used to reset color of clockbackground, line 10
+var textHex = 0;
+
 function pageLoaded() {
-  setInterval(updateTime, 1000)
+  setInterval(updateTime, 1000);
+
+  document.getElementById("clock").onmousemove = changeTextColor;
+  document.getElementById("clock").onmouseleave = function() {//resets to bgcolor of reset of the document
+    console.log(string);
+    document.getElementById("clock").style.backgroundColor =  string;//DOES NOT WORK
+  };
+
 }
 function updateTime() {
   var d = new Date();
@@ -9,7 +19,7 @@ function updateTime() {
   var m = d.getMinutes();
   var s = d.getSeconds();
 
-  var string = "#" + h + "" + m + "" + s ;
+  string = "#" + h + "" + m + "" + s
   print(string, "clock");
   updateBgColor(string);
 }
@@ -17,5 +27,11 @@ function print(content, fieldId) {
   document.getElementById(fieldId).innerHTML = content;
 }
 function updateBgColor(hex){
-  document.body.style.backgroundColor = hex; 
+  document.body.style.backgroundColor = hex;
+}
+function changeTextColor(evt) {
+  evt.target.style.color = "#" + textHex.toString();
+  evt.target.style.backgroundColor = "#" + (textHex.toString() - 350);
+
+  textHex += 500;
 }
